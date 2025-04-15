80a1f2a2b927d71bb1c07959c13f4b219305a5a4
Himansa20 <138608513+Himansa20@users.noreply.github.com>
2025-04-15 21:42:30 +0530

    Project Create

---
+++ pos/src/main/java/com/possystem/pos/PointOfSalesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.possystem.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSalesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfSalesSystemApplication.class, args);
+	}
+
+}
