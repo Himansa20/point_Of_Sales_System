04ae6d42122eab6c4c006fd33fe5a20bfe248cbd
Himansa20 <138608513+Himansa20@users.noreply.github.com>
2025-04-19 18:50:37 +0530

    Solved
    done

--- pos/src/main/java/com/possystem/pos/service/UserService.java
+++ pos/src/main/java/com/possystem/pos/service/UserService.java
@@ -1,7 +1,4 @@
-//<<<<<<< HEAD
-//=======
 
-//>>>>>>> fdd7c5254b3082338dc26244341ca468d8964db2
 package com.possystem.pos.service;
 
 import com.possystem.pos.dto.UserDTO;
@@ -17,19 +14,6 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 public class UserService {
-//<<<<<<< HEAD
-
-    @Autowired
-    private UserRepo userRepo;
-
-    @Autowired
-    private ModelMapper modelMapper;
-
-    public List<UserDTO> getAlluser() {
-        List<User> UserList = userRepo.findAll();
-        return modelMapper.map(UserList, new TypeToken<List<UserDTO>>() {
-        }.getType());
-    }
 
     public UserDTO saveUser(UserDTO userDTO) {
         userRepo.save(modelMapper.map(userDTO, User.class));
@@ -49,7 +33,7 @@ public class UserService {
         User user = userRepo.getUserById(userId);
         return modelMapper.map(user,UserDTO.class);
         }
-//=======
+
  @Autowired
  private UserRepo userRepo;
  
@@ -61,5 +45,5 @@ public class UserService {
   return modelMapper.map(UserList, new TypeToken<List<UserDTO>>(){
   }.getType());
   }
-//>>>>>>> fdd7c5254b3082338dc26244341ca468d8964db2
+
 }
