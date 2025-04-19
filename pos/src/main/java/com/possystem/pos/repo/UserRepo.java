2ab4344b7ce2e130bc8ecd4590cb2fd4f045be55
Himansa20 <138608513+Himansa20@users.noreply.github.com>
2025-04-19 19:05:43 +0530

    update

--- pos/src/main/java/com/possystem/pos/repo/UserRepo.java
+++ pos/src/main/java/com/possystem/pos/repo/UserRepo.java
@@ -3,19 +3,15 @@ package com.possystem.pos.repo;
 
 import com.possystem.pos.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-//<<<<<<< HEAD
+
 import org.springframework.data.jpa.repository.Query;
-//=======
-//>>>>>>> fdd7c5254b3082338dc26244341ca468d8964db2
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends JpaRepository<User,Integer>{
-//<<<<<<< HEAD
 
     @Query(value="SELECT * FROM User WHERE id=?1", nativeQuery=true)
     User getUserById(Integer userId);
-//=======
-//    
-//>>>>>>> fdd7c5254b3082338dc26244341ca468d8964db2
+
 }
